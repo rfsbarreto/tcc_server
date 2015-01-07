8e4221fa6aea64ed8f0b78a183dc715518c89ff3
Raphael Barreto <rfsbarreto@gmail.com>
2015-01-06 23:34:16 -0300

    Added: Model for Ponto

--- tccserver/webservice/models.py
+++ tccserver/webservice/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Ponto(models.Model):
+	latitude = models.CharField(max_length=20)
+	longitude = models.CharField(max_length=20)
+	id_smtt= models.IntegerField()
+	cod_opcao = models.IntegerField()
